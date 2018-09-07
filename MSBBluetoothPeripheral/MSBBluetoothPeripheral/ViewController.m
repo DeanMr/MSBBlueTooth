e00ce5e98dce32d814c52b8e5058154f2b95224a
卢志卫 <deanlzwei@gmail.com>
2018-09-07 18:46:26 +0800

    创建外设，并实现广播服务及发送数据

--- MSBBluetoothPeripheral/MSBBluetoothPeripheral/ViewController.m
+++ MSBBluetoothPeripheral/MSBBluetoothPeripheral/ViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface ViewController ()
+#define SERVICE_UUID @"CDD1"
+#define CHARACTERISTIC_UUID @"CDD2"
+@interface ViewController ()<CBPeripheralManagerDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
+
+@property (nonatomic, strong) CBPeripheralManager *peripheralManager;
+@property (nonatomic, strong) CBMutableCharacteristic *characteristic;
 @end
 
 @implementation ViewController
@@ -17,12 +24,87 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"蓝牙外设";
+    
+    // 创建外设管理器，会回调peripheralManagerDidUpdateState方法
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:dispatch_get_main_queue()];
+    
 }
 
+/** 设备的蓝牙状态
+ CBManagerStateUnknown = 0,  未知
+ CBManagerStateResetting,    重置中
+ CBManagerStateUnsupported,  不支持
+ CBManagerStateUnauthorized, 未验证
+ CBManagerStatePoweredOff,   未启动
+ CBManagerStatePoweredOn,    可用
+ */
+- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
+    if (peripheral.state == CBManagerStatePoweredOn) {
+        // 创建Service（服务）和Characteristics（特征）
+        [self setupServiceAndCharacteristics];
+        // 根据服务的UUID开始广播
+        [self.peripheralManager startAdvertising:@{CBAdvertisementDataServiceUUIDsKey:@[[CBUUID UUIDWithString:SERVICE_UUID]]}];
+    }
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/** 创建服务和特征 */
+- (void)setupServiceAndCharacteristics {
+    // 创建服务
+    CBUUID *serviceID = [CBUUID UUIDWithString:SERVICE_UUID];
+    CBMutableService *service = [[CBMutableService alloc] initWithType:serviceID primary:YES];
+    // 创建服务中的特征
+    CBUUID *characteristicID = [CBUUID UUIDWithString:CHARACTERISTIC_UUID];
+    CBMutableCharacteristic *characteristic = [
+                                               [CBMutableCharacteristic alloc]
+                                               initWithType:characteristicID
+                                               properties:
+                                               CBCharacteristicPropertyRead |
+                                               CBCharacteristicPropertyWrite |
+                                               CBCharacteristicPropertyNotify
+                                               value:nil
+                                               permissions:CBAttributePermissionsReadable |
+                                               CBAttributePermissionsWriteable
+                                               ];
+    // 特征添加进服务
+    service.characteristics = @[characteristic];
+    // 服务加入管理
+    [self.peripheralManager addService:service];
+    
+    // 为了手动给中心设备发送数据
+    self.characteristic = characteristic;
+}
+
+/** 中心设备读取数据的时候回调 */
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveReadRequest:(CBATTRequest *)request {
+    // 请求中的数据，这里把文本框中的数据发给中心设备
+    request.value = [self.textField.text dataUsingEncoding:NSUTF8StringEncoding];
+    // 成功响应请求
+    [peripheral respondToRequest:request withResult:CBATTErrorSuccess];
+}
+
+/** 中心设备写入数据的时候回调 */
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveWriteRequests:(NSArray<CBATTRequest *> *)requests {
+    // 写入数据的请求
+    CBATTRequest *request = requests.lastObject;
+    
+    NSLog(@"接收到数据%@",request.value);
+    //把写入的数据显示在文本框中
+    self.textField.text = [[NSString alloc] initWithData:request.value encoding:NSUTF8StringEncoding];
+}
+
+/** 订阅成功回调 */
+-(void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic {
+    NSLog(@"%s",__FUNCTION__);
+}
+
+/** 取消订阅回调 */
+-(void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic {
+    NSLog(@"%s",__FUNCTION__);
+}
+
+/** 通过固定的特征发送数据到中心设备 */
+- (IBAction)didClickPost:(id)sender {
 }
 
 
